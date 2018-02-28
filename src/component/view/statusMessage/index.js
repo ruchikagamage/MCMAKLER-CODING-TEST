28d0c3818bc39abdfd76e777d4bbba6600879a56
ruchikagamage <ruchi.newonline@gmail.com>
2018-02-28 13:37:59 +0530

    READ ME update

--- src/component/view/statusMessage/index.js
+++ src/component/view/statusMessage/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./styles.css";
-import error from "./../../../img/disconnected-chains.svg";
+import error from "./../../../img/error.svg";
 import warning from "./../../../img/warning.svg";
 import noImage from "./../../../img/error-404.svg";
+import info from "./../../../img/information.svg";
+import success from "./../../../img/success.svg";
 
 import classNames from "classnames";
 
@@ -13,15 +15,19 @@ class StatusMessage extends Component {
       img = error;
     } else if (this.props.status === "WARNING") {
       img = warning;
+    } else if (this.props.status === "INFO") {
+      img = info;
+    } else if (this.props.status === "SUCCESS") {
+      img = success;
     } else {
       img = noImage;
     }
 
-    let messageText = classNames("font-medium", {
+    let messageText = classNames("font-medium", "margin-top-5", {
       "status-message-error": this.props.status === "ERROR",
       "status-message-warning": this.props.status === "WARNING",
       "status-message-info": this.props.status === "INFO",
-      "status-message-sucess": this.props.status === "SUCESS"
+      "status-message-success": this.props.status === "SUCCESS"
     });
     return (
       <div className="status-message">
